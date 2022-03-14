85d0ff5cdd971b5e11feb665fec72a99c9b2e82b
nelson6868 <ekwomnelson090@gmail.com>
2022-03-14 09:01:20 +0100

    I worked on menuItems Model

---
+++ Tangy/Controllers/CouponsController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Tangy.Data;
+using Tangy.Models;
+
+namespace Tangy.Controllers
+{
+    public class CouponsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CouponsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Coupons.ToListAsync());
+        }
+
+
+        //GET Coupons
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST Create Coupons
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Coupons coupons)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files[0] != null && files[0].Length > 0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+                    coupons.Picture = p1;
+                    _db.Coupons.Add(coupons);
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(coupons);
+        }
+
+
+
+    }
+}
